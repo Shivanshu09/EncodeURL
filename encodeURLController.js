f8ef21c6a2f2fda0b358cfbd7a021534e8e0286a
Shivanshu Ahuja <47395416+Shivanshu09@users.noreply.github.com>
2021-09-20 18:31:51 +0530

    Update encodeURLController.js

--- encodeURLController.js
+++ encodeURLController.js
@@ -3,9 +3,7 @@ const bcrypt = require('bcrypt');
 
 module.exports.encode = (req, res) => {
 
-   
     const url = '"upi://pay?pa=bivek@npci&pn=bivek%20rath&mc=9999&tid=cxnkjcnkjdfdvjndkjfvn&tr=4894398cndhcd23&tn=Pay%20to%20mystar%20store&am=10&mam=null&cu=INR&url=https://mystar.com&mode=05&&orgid=000000&mid=1234&msid=3432&mtid=1212';
-
 
     const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
         modulusLength: 2048,
@@ -18,7 +16,6 @@ module.exports.encode = (req, res) => {
 
     const encodedSignature = signature.toString("base64");
     const finalURL = url + '&sign=' + encodedSignature;
-    
       
     console.log(finalURL);
     res.send(finalURL);
