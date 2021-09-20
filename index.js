857fbf44d2c7995f6c03aff069dd6645546f6ddb
Shivanshu Ahuja <47395416+Shivanshu09@users.noreply.github.com>
2021-09-20 18:32:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-const encodeURL = require('./routes/encodeUrlRoute');
+const encodeURL = require('./encodeUrlRoute');
 
 app.use('/generateKey', encodeURL);
 app.listen(3000);
